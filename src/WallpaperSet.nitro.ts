5658f55c19d7cf57bd2b0d8e2b967597c0f06aec
rahulmandyal1 <rahulmandyal079@gmail.com>
2026-01-05 19:29:06 +0530

    feat: update API to support home, lock, and both screens

--- src/WallpaperSet.nitro.ts
+++ src/WallpaperSet.nitro.ts
@@ -1,5 +1,5 @@
 import type { HybridObject } from 'react-native-nitro-modules';
 
 export interface WallpaperSet extends HybridObject<{ android: 'kotlin' }> {
-  setWallpaper(image: string): Promise<void>;
+  setWallpaper(image: string, location: number): Promise<void>;
 }
